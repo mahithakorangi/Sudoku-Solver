312df1ea77e72fc94d0f6db1351fef6c98c46396
mahithakorangi <mahithakorangi@gmail.com>
2025-10-03 23:48:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Sudoku-Solver
+# Sudoku Solver using Backtracking
+
+## Project Idea
+This project solves a standard 9x9 Sudoku puzzle using the Backtracking algorithm. The board is represented as a 2D list, and the algorithm fills empty cells (represented by 0) to complete the puzzle.
+
+## Pseudo-code
---
+++ sudoku solver.py
@@ -0,0 +1,75 @@
+
+
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
+
+app = Flask(__name__)
+
+# --- Backend: Sudoku Solver ---
+def is_valid(board, row, col, num):
+    for x in range(9):
+        if board[row][x] == num:
+            return False
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[startRow + i][startCol + j] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/solve', methods=['POST'])
+def solve():
+    board = []
+    for i in range(9):
+        row = []
+        for j in range(9):
+            val = request.form.get(f'cell{i}_{j}', '')
+            try:
+                num = int(val)
+                if 1 <= num <= 9:
+                    row.append(num)
+                else:
+                    row.append(0)
+            except:
+                row.append(0)
+        board.append(row)
+    original = [r[:] for r in board]
+    if solve_sudoku(board):
+        return render_template('solution.html', solution=board)
+    else:
+        return '<h3>No solution exists. <a href="/">Try again</a></h3>'
+
+@app.route('/record', methods=['POST'])
+def record():
+    solution_json = request.form.get('solution')
+    solution = json.loads(solution_json)
+    # Save to file
+    record_path = os.path.join(os.path.dirname(__file__), 'recorded_solution.txt')
+    with open(record_path, 'a') as f:
+        f.write(json.dumps(solution) + '\n')
+    return '<h3>Solution recorded! <a href="/">Back to Home</a></h3>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
